9eb3ed268213e9e7590f90319b8667e7b2a49dd3
Kike Navas <enrique.navas@uvic.cat>
2025-01-27 17:49:45 +0100

    New feature and parser

--- seqClass.py
+++ seqClass.py
@@ -5,12 +5,21 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
 args = parser.parse_args()
+
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
 
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
